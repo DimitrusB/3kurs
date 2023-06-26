846ede6de3ebac3b7583e821e72b9fed4c7e41cf
DimitrusB <dimitrusboss@mail.ru>
2023-06-26 23:00:11 +0300

    добавление таймера

--- index.js
+++ index.js
@@ -35,9 +35,29 @@ const renderChoosePage = ()=>{
 
     chooseButton.addEventListener('click', (event) =>{
         event.preventDefault();
+
+         let time = 0;
+     
+         function updateTime() {
+            time++;
+            const minutes = Math.floor(time / 60);
+            const seconds = Math.floor(time % 60);
+            const formattedTime = pad(minutes) + ':' + pad(seconds);
+            timer.textContent = formattedTime;
+          }
+
+        function pad(value) {
+            return value < 10 ? '0' + value : value;
+          }
+
+        setInterval(updateTime, 1000);
+    
         if (easyLevel.checked){
         const renderEasyPage =
-            `<div class="center">
+            `<span class="timerMin">min</span>
+            <span class="timerSec">sec</span>
+            <div id="timer" class="timer">00:00</div>
+            <div class="center">
             <div  class="main__window_choose">
             <input type="radio" id="option1" name="options" value="Option 1" > 
             <label for="option1">1</label>
@@ -45,10 +65,16 @@ const renderChoosePage = ()=>{
              </div>
             `
         appEl.innerHTML=renderEasyPage;
-        }
+        const timer = document.getElementById('timer');
+
+        updateTime();
+          }
         else if (middleLevel.checked){
             const renderMiddlePage =
-                `<div class="center">
+                `<span class="timerMin">min</span>
+                <span class="timerSec">sec</span>
+                <div id="timer" class="timer">00:00</div>
+                <div class="center">
                 <div  class="main__window_choose">
                 <input type="radio" id="option2" name="options" value="Option 2" >
                 <label for="option2">2</label>
@@ -59,7 +85,10 @@ const renderChoosePage = ()=>{
             }
         else if (hardLevel.checked){
             const renderHardPage =
-                    `<div class="center">
+                    `<span class="timerMin">min</span>
+                    <span class="timerSec">sec</span>
+                    <div id="timer" class="timer">00:00</div>
+                    <div class="center">
                     <div  class="main__window_choose">
                     <input type="radio" id="option3" name="options" value="Option 3">
                     <label for="option3">3</label>
