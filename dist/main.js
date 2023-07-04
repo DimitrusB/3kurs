766771bfb32617df1d8bc5ca7cafed13c4909fe7
dmitriy <admin@silverboats.ru>
2023-07-04 13:05:53 +0300

    formatter

--- dist/main.js
+++ dist/main.js
@@ -10,23 +10,23 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./index.js":
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modulFunc_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulFunc.js */ \"./modulFunc.js\");\n\r\n\r\n\r\n\r\nconst renderChoosePage = ()=>{\r\n    const appEl = document.getElementById('app');\r\n\r\n    const choosePageHtml = `\r\n    <div class=\"center\">\r\n    <div class=\"main__window\">\r\n        <div class=\"main__window_senc\">\r\n            <p>Выбери сложность</p>\r\n        </div>\r\n        <form class=\"main__window_form\">\r\n            <div  class=\"main__window_choose\">\r\n            <input type=\"radio\" id=\"option1\" name=\"options\" value=\"Option 1\" > \r\n            <label for=\"option1\">1</label>\r\n            </div>\r\n            <div  class=\"main__window_choose\">\r\n            <input type=\"radio\" id=\"option2\" name=\"options\" value=\"Option 2\" >\r\n            <label for=\"option2\">2</label>\r\n            </div>\r\n            <div  class=\"main__window_choose\">\r\n            <input type=\"radio\" id=\"option3\" name=\"options\" value=\"Option 3\">\r\n            <label for=\"option3\">3</label>\r\n            </div>\r\n        </form>\r\n            <button class=\"main__window_butt\" id=\"chooseButton\">Старт</button>\r\n    </div>\r\n    `\r\n    appEl.innerHTML = choosePageHtml;\r\n\r\n    const easyLevel = document.getElementById('option1');\r\n    const middleLevel = document.getElementById('option2');\r\n    const hardLevel = document.getElementById('option3');\r\n    const chooseButton = document.getElementById('chooseButton');\r\n\r\n    function renderLevel(level) {\r\n        const renderLevelPage = `\r\n          <div>\r\n            <div>\r\n              <span class=\"timerMin\">min</span>\r\n              <span class=\"timerSec\">sec</span>\r\n              <button class=\"game_butt\" id=\"startGame\">Начать заново</button>\r\n            </div>\r\n            <div id=\"timer\" class=\"timer\">00:00</div>\r\n          </div>\r\n          <div class=\"center\">\r\n            <form class=\"main__window_choose\">\r\n              <input type=\"radio\" id=\"option${level}\" name=\"options\" value=\"Option ${level}\">\r\n              <label for=\"option${level}\">${level}</label>\r\n            </form>\r\n          </div>\r\n        `;\r\n        \r\n        appEl.innerHTML = renderLevelPage;\r\n        const timer = document.getElementById('timer');\r\n        const goBegin = document.getElementById('startGame');\r\n        (0,_modulFunc_js__WEBPACK_IMPORTED_MODULE_0__.timerSet)(timer);\r\n        \r\n        goBegin.addEventListener('click', () => {\r\n          renderChoosePage();\r\n        });\r\n      }\r\n      chooseButton.addEventListener('click', (event) => {\r\n        event.preventDefault();\r\n      \r\n      let level = '';\r\n      \r\n      if (easyLevel.checked){\r\n          level = '1';\r\n        }else if (middleLevel.checked){\r\n          level = '2';\r\n        }\r\n        else if (hardLevel.checked){\r\n          level ='3';\r\n        }\r\n        else if (!level) {\r\n          alert('Выберите уровень сложности!!!')\r\n          return;\r\n        }\r\n        \r\n        renderLevel(level);\r\n      });\r\n}\r\nrenderChoosePage();\r\n\n\n//# sourceURL=webpack://3kurs/./index.js?");
-
-/***/ }),
-
-/***/ "./modulFunc.js":
+/***/ "./src/index.js":
 /*!**********************!*\
-  !*** ./modulFunc.js ***!
+  !*** ./src/index.js ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   timerSet: () => (/* binding */ timerSet)\n/* harmony export */ });\n\r\nfunction timerSet(timer){\r\n    let time = 0;\r\nfunction updateTime() {\r\n\r\n    time++;\r\n    const minutes = Math.floor(time / 60);\r\n    const seconds = Math.floor(time % 60);\r\n    const formattedTime = pad(minutes) + ':' + pad(seconds);\r\n    timer.textContent = formattedTime;\r\n  }\r\n\r\n  function pad(value) {\r\n    return value < 10 ? '0' + value : value;\r\n  }\r\n  setInterval(updateTime, 1000);\r\n  updateTime();\r\n}\r\n\n\n//# sourceURL=webpack://3kurs/./modulFunc.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modulFunc_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulFunc.js */ \"./src/modulFunc.js\");\n\n\nconst renderChoosePage = () => {\n    const appEl = document.getElementById('app')\n\n    const choosePageHtml = `\n    <div class=\"center\">\n    <div class=\"main__window\">\n        <div class=\"main__window_senc\">\n            <p>Выбери сложность</p>\n        </div>\n        <form class=\"main__window_form\">\n            <div  class=\"main__window_choose\">\n            <input type=\"radio\" id=\"option1\" name=\"options\" value=\"Option 1\" > \n            <label for=\"option1\">1</label>\n            </div>\n            <div  class=\"main__window_choose\">\n            <input type=\"radio\" id=\"option2\" name=\"options\" value=\"Option 2\" >\n            <label for=\"option2\">2</label>\n            </div>\n            <div  class=\"main__window_choose\">\n            <input type=\"radio\" id=\"option3\" name=\"options\" value=\"Option 3\">\n            <label for=\"option3\">3</label>\n            </div>\n        </form>\n            <button class=\"main__window_butt\" id=\"chooseButton\">Старт</button>\n    </div>\n    `\n    appEl.innerHTML = choosePageHtml\n\n    const easyLevel = document.getElementById('option1')\n    const middleLevel = document.getElementById('option2')\n    const hardLevel = document.getElementById('option3')\n    const chooseButton = document.getElementById('chooseButton')\n\n    function renderLevel(level) {\n        const renderLevelPage = `\n          <div>\n            <div>\n              <span class=\"timerMin\">min</span>\n              <span class=\"timerSec\">sec</span>\n              <button class=\"game_butt\" id=\"startGame\">Начать заново</button>\n            </div>\n            <div id=\"timer\" class=\"timer\">00:00</div>\n          </div>\n          <div class=\"center\">\n            <form class=\"main__window_choose\">\n              <input type=\"radio\" id=\"option${level}\" name=\"options\" value=\"Option ${level}\">\n              <label for=\"option${level}\">${level}</label>\n            </form>\n          </div>\n        `\n\n        appEl.innerHTML = renderLevelPage\n        const timer = document.getElementById('timer')\n        const goBegin = document.getElementById('startGame')\n        ;(0,_modulFunc_js__WEBPACK_IMPORTED_MODULE_0__.timerSet)(timer)\n\n        goBegin.addEventListener('click', () => {\n            renderChoosePage()\n        })\n    }\n\n    chooseButton.addEventListener('click', (event) => {\n        event.preventDefault()\n\n        let level = ''\n\n        if (easyLevel.checked) {\n            level = '1'\n        } else if (middleLevel.checked) {\n            level = '2'\n        } else if (hardLevel.checked) {\n            level = '3'\n        } else if (!level) {\n            alert('Выберите уровень сложности!!!')\n            return\n        }\n\n        renderLevel(level)\n    })\n}\nrenderChoosePage()\n\n\n//# sourceURL=webpack://3kurs/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modulFunc.js":
+/*!**************************!*\
+  !*** ./src/modulFunc.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   timerSet: () => (/* binding */ timerSet)\n/* harmony export */ });\nfunction timerSet(timer) {\n    let time = 0\n    function updateTime() {\n        time++\n        const minutes = Math.floor(time / 60)\n        const seconds = Math.floor(time % 60)\n        const formattedTime = pad(minutes) + ':' + pad(seconds)\n        timer.textContent = formattedTime\n    }\n\n    function pad(value) {\n        return value < 10 ? '0' + value : value\n    }\n    setInterval(updateTime, 1000)\n    updateTime()\n}\n\n\n//# sourceURL=webpack://3kurs/./src/modulFunc.js?");
 
 /***/ })
 
@@ -90,7 +90,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { timerSet } from "./modulFunc.js";
+import { timerSet } from './modulFunc.js'
 
-
-
-const renderChoosePage = ()=>{
-    const appEl = document.getElementById('app');
+const renderChoosePage = () => {
+    const appEl = document.getElementById('app')
 
     const choosePageHtml = `
     <div class="center">
@@ -28,12 +26,12 @@ const renderChoosePage = ()=>{
             <button class="main__window_butt" id="chooseButton">Старт</button>
     </div>
     `
-    appEl.innerHTML = choosePageHtml;
+    appEl.innerHTML = choosePageHtml
 
-    const easyLevel = document.getElementById('option1');
-    const middleLevel = document.getElementById('option2');
-    const hardLevel = document.getElementById('option3');
-    const chooseButton = document.getElementById('chooseButton');
+    const easyLevel = document.getElementById('option1')
+    const middleLevel = document.getElementById('option2')
+    const hardLevel = document.getElementById('option3')
+    const chooseButton = document.getElementById('chooseButton')
 
     function renderLevel(level) {
         const renderLevelPage = `
@@ -51,36 +49,35 @@ const renderChoosePage = ()=>{
               <label for="option${level}">${level}</label>
             </form>
           </div>
-        `;
-        
-        appEl.innerHTML = renderLevelPage;
-        const timer = document.getElementById('timer');
-        const goBegin = document.getElementById('startGame');
-        timerSet(timer);
-        
+        `
+
+        appEl.innerHTML = renderLevelPage
+        const timer = document.getElementById('timer')
+        const goBegin = document.getElementById('startGame')
+        timerSet(timer)
+
         goBegin.addEventListener('click', () => {
-          renderChoosePage();
-        });
-      }
-      chooseButton.addEventListener('click', (event) => {
-        event.preventDefault();
-      
-      let level = '';
-      
-      if (easyLevel.checked){
-          level = '1';
-        }else if (middleLevel.checked){
-          level = '2';
+            renderChoosePage()
+        })
+    }
+
+    chooseButton.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        let level = ''
+
+        if (easyLevel.checked) {
+            level = '1'
+        } else if (middleLevel.checked) {
+            level = '2'
+        } else if (hardLevel.checked) {
+            level = '3'
+        } else if (!level) {
+            alert('Выберите уровень сложности!!!')
+            return
         }
-        else if (hardLevel.checked){
-          level ='3';
-        }
-        else if (!level) {
-          alert('Выберите уровень сложности!!!')
-          return;
-        }
-        
-        renderLevel(level);
-      });
+
+        renderLevel(level)
+    })
 }
-renderChoosePage();
+renderChoosePage()
